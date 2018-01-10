c813070033d54be088c660858e5804dcea360fdb
Cam Starts <camstarts2@gmail.com>
2018-01-11 06:58:24 +0700

    Create new Image Slider View for Android Libray

---
+++ app/src/main/java/com/camstarts/imagesliderview/MainSliderActivity.java
@@ -0,0 +1,13 @@
+package com.camstarts.imagesliderview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSliderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_slider);
+    }
+}
